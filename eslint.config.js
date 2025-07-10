229792ab0b936d15f95a8fa0c8452aa9e4831053
maynormoe <maynormoe0@gmail.com>
2025-07-10 16:59:12 +0800

    chore: 🔨 修改eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,16 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "src-tauri"] },
+  {
+    ignores: [
+      "dist",
+      "src-tauri",
+      "build",
+      "node_modules",
+      "*.config.js",
+      "*.config.ts",
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
@@ -19,18 +28,7 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-      ],
-      "@typescript-eslint/explicit-function-return-type": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
-      "prefer-const": "error",
-      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-empty-pattern": "off",
     },
   }
 );
